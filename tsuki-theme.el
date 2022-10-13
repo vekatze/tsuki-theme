494cf0f3dbf3b41980a9dec2aaeed9e22f187a09
vekatze <vekatze@icloud.com>
2022-10-13 15:43:38 +0900

    autoload

--- tsuki-theme.el
+++ tsuki-theme.el
@@ -7,6 +7,13 @@
 ;; A color theme for Emacs.
 ;;; Code:
 
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (defun tsuki-theme--semiquote (ast)
   (if (and (listp ast) (not (eq (car ast) 'quote)))
       (cons 'list (mapcar #'tsuki-theme--semiquote ast))
